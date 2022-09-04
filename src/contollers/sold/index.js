0327833950cc2748d8f5bb291b251bc41965dcf5
Roiner Bracamonte <rbracamonte.winhold@gmail.com>
2022-09-04 05:58:24 -0500

    fix

--- src/contollers/sold/index.js
+++ src/contollers/sold/index.js
@@ -1,15 +1,15 @@
 const { users } = require("../../../data");
 const registerSold = (req, res, next) => {
-  const { caja, gastos, contactPhoneNumber } = req.body;
+  const { caja, gasto, contactPhoneNumber } = req.body;
   const { body } = req;
-  const sold = parseInt(caja) + parseInt(gastos);
+  const sold = parseInt(caja) + parseInt(gasto);
   let key;
   for (let i = 0; i < users.length; i++) {
     if (users[i].contactPhoneNumber == contactPhoneNumber) {
       key = i;
     }
   }
-  users.push({ ...body, sold: sold });
+  users.push({ sold: sold });
   res.json({ res: sold });
 };
 
